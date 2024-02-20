02ddff27b0a258b412804e7bc6a4da7327a4eb36
unisalledatos <123432461+unisalledatos@users.noreply.github.com>
2024-02-20 11:07:54 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from sklearn.linear_model import LinearRegression
+import pickle
+
+st.title("Wage prediction")
+
+with open('model.pickle', 'rb') as f:
+  modelo = pickle.load(f)
+
+educ = st.slider("Seleccione educación", 0, 20)
+exper = st.slider('Seleccione experiencia", 0, 50)
+
+vec = np.array([[1, educ, exper]])
+
+if st.button("Predecir salario"):
+  st.write(modelo.predict(vec))
